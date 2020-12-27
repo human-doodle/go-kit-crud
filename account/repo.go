5fecab3cab138dd5cdf6d3d4d9df94c5424d84d6
apoopan <apoorvapandey98@gmail.com>
2020-12-27 23:46:10 +0530

    Updates get user

--- account/repo.go
+++ account/repo.go
@@ -56,7 +56,7 @@ func (repo *repo) GetUser(ctx context.Context, id string) (profile Profile, errr
 func (repo *repo) GetUser(ctx context.Context) (interface{}, error) {
 	coll := repo.db.C("bloguser")
 	data := []User{}
-	err := coll.Find(bson.M{}).Select(bson.M{"email": 1, "city": 1, "age": 1}).All(&data)
+	err := coll.Find(bson.M{}).All(&data)
 	if err != nil {
 		fmt.Println("Error occured inside GetCUstomerById in repo")
 		return "", err
